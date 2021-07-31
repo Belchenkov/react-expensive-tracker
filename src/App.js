ee37038104fb20fbf3c0686f7c81de7b7c93cad8
Aleksey Belchenkov <u608110@gmail.com>
2021-07-31 18:44:54 +0400

    Add Expenses List

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+    { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2020, 6, 14) },
+    { id: 'e2', title: 'New TV', amount: 124.07, date: new Date(2021, 5, 4) },
+    { id: 'e3', title: 'Toilet Paper', amount: 34.57, date: new Date(2029, 4, 5) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 145.52, date: new Date(2021, 3, 8) },
+];
+
 function App() {
-    const expenses = [
-        { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 6, 14) },
-        { title: 'New TV', amount: 124.07, date: new Date(2021, 5, 4) },
-        { title: 'Toilet Paper', amount: 34.57, date: new Date(2021, 4, 5) },
-        { title: 'New Desk (Wooden)', amount: 145.52, date: new Date(2021, 3, 8) },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = expense => {
-        console.log(expense, 'App');
+        setExpenses(prevExpenses => {
+            return [
+                expense,
+                ...expenses
+            ]
+        });
     };
 
     return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from './ExpensesFilter';
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
 
 const Expenses = ({ items }) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2021');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <Card className="expenses">
@@ -18,17 +22,8 @@ const Expenses = ({ items }) => {
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
-            {
-                items && items.map(expense => (
-                    <ExpenseItem
-                        key={expense.date}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))
-            }
-        < /Card>
+            <ExpensesList items={filteredExpenses}/>
+        </ Card>
     );
 };
 
